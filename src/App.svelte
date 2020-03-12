89fb639047bfb0a77563a3333db739a0902b2e9b
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-12 11:27:43 -0400

    Create the is Bourbon street clean site using svelte

---
+++ src/App.svelte
@@ -0,0 +1,100 @@
+<script>
+  import { onMount } from "svelte";
+  import moment from "moment-timezone";
+  import Footer from "./Footer.svelte";
+  let answer = "";
+
+  let current = moment.tz("America/Chicago");
+  $: currentTime = current.format("LT");
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      current = moment.tz("America/Chicago");
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  $: if (current.hour() >= 23 || current.hour() < 6) {
+    if (current.hour() >= 0 && current.hour() < 3) {
+      answer = "Fuck no";
+    } else if (current.hour() >= 3 && current.hour() < 6) {
+      answer = "🤢🤢🤢";
+    }
+  } else if (current.hour() >= 6 && current.hour() < 10) {
+    answer = "So clean you could eat off the ground";
+  } else if (current.hour() >= 10 && current.hour() < 18) {
+    answer = "So .. Heck yeah!";
+  } else if (current.hour() >= 18 && current.hour() < 22) {
+    answer = "¯\\_(ツ)_/¯. I guess...";
+  }
+</script>
+
+<style>
+  main {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url("../images/bourbon-street-day.jpg");
+    height: 100%;
+
+    /* Position and center the image to scale nicely on all screens */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    /* font stuff */
+    font-family: "Overpass", sans-serif;
+    text-align: center;
+    /* max-width: 240px; */
+    margin: 0 auto;
+  }
+
+  /* Place text in the middle of the image */
+  .hero-text {
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -20%);
+    color: white;
+  }
+
+  h1 {
+    /* color: #000; */
+    font-size: 4em;
+    font-weight: 700;
+    font-style: italic;
+  }
+
+  p {
+    font-size: 2em;
+    font-weight: 400;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  @media (max-width: 640px) {
+    h1 {
+      font-size: 2.75em;
+    }
+
+    p {
+      font-size: 1.5em;
+    }
+  }
+</style>
+
+<main>
+  <div class="hero-text">
+    <h1>Is Bourbon Street Clean Now?</h1>
+    <p>It's {currentTime} in New Orleans</p>
+    <p>{answer}</p>
+  </div>
+</main>
+<Footer />
---
+++ src/Footer.svelte
@@ -0,0 +1,32 @@
+<style>
+  footer {
+    color: aliceblue;
+    position: absolute;
+    bottom: 2px;
+    width: 100%;
+    text-align: center;
+    align-self: center;
+    font-family: "Rubik", sans-serif;
+    font-size: 1.25em;
+  }
+
+  @media (max-width: 640px) {
+    footer {
+      font-size: small;
+    }
+  }
+</style>
+
+<footer>
+  &copy; 2020. Built by
+  <a
+    title="My Github Profile"
+    href="https://github.com/itsrainingmani"
+    style="color: #3182ce;">
+    <i>@itsrainingmani</i>
+  </a>
+  using
+  <a title="Svelte Website" href="https://svelte.dev" style="color: #e53e3e;">
+    <i>Svelte</i>
+  </a>
+</footer>
